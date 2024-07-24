88d2e978ebf4b4e79d65c94adf03bea5ee05c14f
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-07-24 12:50:59 -0500

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/connection-db";
+import { client } from "./config/eureka-client";
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,17 @@ app.listen(7000, () => {
   console.log("Server started on port 7000");
 });
 
-export default app;
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source connected!");
+
+    app.listen(7000, () => {
+      console.log("Server started on port 7000");
+      client.start((error) => {
+        console.log(error || "Eureka client started");
+      });
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to the database", err);
+  });
